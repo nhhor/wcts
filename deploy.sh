45fb315e544768bb093b9af9a4bb315f054e55ae
nhhor <nhhor@post.com>
2026-04-23 18:45:17 -0700

    update deploy.sh 2

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f origin main:gh-pages
+# git push -f origin main:gh-pages # wasn't working, so using subtree instead:
+git subtree push --prefix dist origin gh-pages
 
 cd -
